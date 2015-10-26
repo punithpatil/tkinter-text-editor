1e445ffc26a5aaa76486f8cf3fde26cd9b9e079e
Punith Patil <punithp007@gmail.com>
2015-10-26 11:39:16 +0530

    Changes in main to suit edit_menu.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from common_imports import *
 from tkFileDialog import *
 from tkMessageBox import *
+from tkFont import Font
+from ScrolledText import *
 import file_menu
 import edit_menu
-root = Tk()
-root.title("My Python Text Editor")
-root.minsize(width=400, height=400)
-root.maxsize(width=400, height=400)
 
-text = Text(root, width=400, height=400)
-text.pack()
+root = Tk()
+
+root.title("Text Editor-Untiltled")
+root.geometry("300x250+300+300")
+root.minsize(width=400, height=400)
+
+font = Font(family="Verdana", size=10)
+
+text = ScrolledText(root, state='normal', height=400, width=400, wrap='word', font = font, pady=2, padx=3, undo=True)
+text.pack(fill=Y, expand=1)
+text.focus_set()
 
 menubar = Menu(root)
 
