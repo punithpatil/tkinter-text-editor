607ff0a2afe8d28610ec5451b698ff3dd16be675
Punith Patil <punithp007@gmail.com>
2015-10-26 12:50:29 +0530

    Modified main.py to support format_menu.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tkFont import Font
 from ScrolledText import *
 import file_menu
 import edit_menu
+import format_menu
 import help_menu
 
 root = Tk()
@@ -23,5 +24,6 @@ menubar = Menu(root)
 
 file_menu.main(root,text,menubar)
 edit_menu.main(root,text,menubar)
+format_menu.main(root,text,menubar)
 help_menu.main(root,text,menubar)
 root.mainloop()
